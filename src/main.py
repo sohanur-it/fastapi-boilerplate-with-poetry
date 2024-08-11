f718a4eb49ca8c45b83ba1505d0678fd56f4e312
sohanur rahman <shohanur.rahman@gennext.net>
2024-08-11 12:39:15 +0600

    fastapi boilerplate with poetry

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return {"message": "hello"}
+
+def start():
+    uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
+
+if __name__ == "__main__":
+    start()
